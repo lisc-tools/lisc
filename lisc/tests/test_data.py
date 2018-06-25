8bf830688ea3a473754b9a603e0aad82e35e5fd3
TomDonoghue <tdonoghue@ucsd.edu>
2018-06-24 22:33:13 -0700

    update to use fixture

--- lisc/tests/test_data.py
+++ lisc/tests/test_data.py
@@ -23,69 +23,60 @@ def test_iter():
     for dd in dat:
         assert dd
 
-def test_add_id():
+def test_add_id(tdata):
 
-    dat = load_data()
-    dat.add_id(1)
+    tdata.add_id(1)
 
-    assert dat.ids
+    assert tdata.ids
 
-def test_add_title():
+def test_add_title(tdata):
 
-    dat = load_data()
-    dat.add_title('title')
+    tdata.add_title('title')
 
-    assert dat.titles
+    assert tdata.titles
 
-def test_add_authors():
+def test_add_authors(tdata):
 
-    dat = load_data()
-    dat.add_authors(('Last', 'First', 'IN', 'School'))
+    tdata.add_authors(('Last', 'First', 'IN', 'School'))
 
-    assert dat.authors
+    assert tdata.authors
 
-def test_add_journal():
+def test_add_journal(tdata):
 
-    dat = load_data()
-    dat.add_journal('Journal name', 'J abbrev')
+    tdata.add_journal('Journal name', 'J abbrev')
 
-    assert dat.journals
+    assert tdata.journals
 
-def test_add_words():
+def test_add_words(tdata):
 
-    dat = load_data()
-    dat.add_words(['new', 'dat'])
+    tdata.add_words(['new', 'dat'])
 
-    assert dat.words
+    assert tdata.words
 
-def test_add_kws():
+def test_add_kws(tdata):
 
-    dat = load_data()
-    dat.add_kws(['list', 'of', 'kws'])
+    tdata.add_kws(['list', 'of', 'kws'])
 
-    assert dat.kws
+    assert tdata.kws
 
-def test_add_pub_date():
+def test_add_pub_date(tdata):
 
-    dat = load_data()
-    dat.add_pub_date((2000, 'Feb'))
+    tdata.add_pub_date((2000, 'Feb'))
 
-    assert dat.years
-    assert dat.months
+    assert tdata.years
+    assert tdata.months
 
-def test_add_doi():
+def test_add_doi(tdata):
 
-    dat = load_data()
-    dat.add_doi('doi_str')
+    tdata.add_doi('doi_str')
 
-    assert dat.dois
+    assert tdata.dois
 
-def test_increment_n_articles():
+def test_increment_n_articles(tdata):
 
-    dat = load_data()
-    dat.increment_n_articles()
+    tdata.increment_n_articles()
 
-    assert dat.n_articles
+    assert tdata.n_articles
 
 def test_check_results():
 
