dae3c71e1c0bf57129f15d232804c8b43902ca5a
TomDonoghue <tdonoghue@ucsd.edu>
2018-06-24 22:32:57 -0700

    Update to use return instead of yield. Also add tdata.

--- lisc/tests/conftest.py
+++ lisc/tests/conftest.py
@@ -2,23 +2,23 @@
 
 import pytest
 
-from lisc.tests.utils import load_base
+from lisc.tests.utils import load_base, load_data
 
 ###################################################################################################
 ###################################################################################################
 
 @pytest.fixture(scope='function')
 def tbase_empty():
-    yield load_base()
+    return load_base()
 
 @pytest.fixture(scope='function')
 def tbase_terms():
-    yield load_base(True)
+    return load_base(True)
 
 @pytest.fixture(scope='function')
 def tbase_terms_excl():
-    yield load_base(True, True)
+    return load_base(True, True)
 
-# @pytest.fixture(scope='session')
-# def tfg():
-#     yield get_tfg()
+@pytest.fixture(scope='function')
+def tdata():
+    return load_data()
