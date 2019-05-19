afd4e8501ccaf6c3f0e00799ebd732e9c40ac9b2
TomDonoghue <tdonoghue@ucsd.edu>
2019-05-18 21:22:29 -0700

    Small fixes of tutorials

--- tutorial/plot_02-CountsAnalysis.py
+++ tutorial/plot_02-CountsAnalysis.py
@@ -1,9 +1,12 @@
 """
 Tutorial 02 - Counts Analysis
 =============================
+
+Analyzing scraped co-occurence data.
 """
 
-###############################################################################
+###################################################################################################
+
 #
 # This tutorial explores the built in utilities for exploring & anayzing counts data.
 # TODO
