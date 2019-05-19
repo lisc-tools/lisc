f37a515504f2f67b2d4eb9b8b0983c6d4d6d1e89
TomDonoghue <tdonoghue@ucsd.edu>
2019-05-18 19:58:46 -0700

    Update for API keys & consolidate URL building

--- lisc/core/urls.py
+++ lisc/core/urls.py
@@ -16,11 +16,11 @@ Tools
 EInfo : Provides a list of all databases, and some basic data about them.
     args - db, retmode
 EGQuery : Provides the number of records in all databases by a single query.
-    args - ?
+    args - ToDo
 ESearch : Returns UIDs matching a text query, or posts to / gets from history server.
     args - db, term, field
 EFetch : Returns formatted data records for a list of UIDs.
-    args - ?
+    args - ToDo
 
 Settings
 --------
@@ -39,11 +39,6 @@ usehistory : Whether to store findings on remote server.
 """
 
 from lisc.core.errors import InconsistentDataError
-
-###################################################################################################
-###################################################################################################
-
-AUTH = False
 
 ###################################################################################################
 ###################################################################################################
@@ -67,10 +62,14 @@ class URLS(object):
         Dictionary of all defined settings and their values.
     args : dict()
         Dictionary of all arguments (settings & values) that can be used in e-utils URL.
+    authenticated : boolean
+        Whether using an API key as an authenticated NCBI user.
+    api_key : str
+        The API key, if operating as an authenticated user.
     """
 
     def __init__(self, db=None, usehistory='n', retmax=None, field=None,
-                 retmode=None, auto_gen=False):
+                 retmode=None, auto_gen=False, api_key=None):
         """Initialize the ncbi e-utils urls.
 
         Parameters
@@ -85,11 +84,13 @@ class URLS(object):
             The search field to search within.
         retmode : {'lxml', 'xml'}, optional
             The return format for the results.
-        auto_gen : boolean, optional
+        auto_gen : boolean, optional, default: False
             Whether to automatically generate URLs (without extra arguments).
+        api_key : str, optional
+            An API key for authenticated NCBI user account.
         """
 
-        # Set up the base url for ncbi e-utils
+        # Set up the base url for NCBI E-Utils
         self.eutils = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
 
         # Initialize variables to store search and fetch URLs
@@ -100,20 +101,82 @@ class URLS(object):
 
         # Initialize dictionary to save settings, and add settings to it
         self.settings = dict()
-        self.save_settings(db=db, usehistory=usehistory, retmax=retmax,
+        self.fill_settings(db=db, usehistory=usehistory, retmax=retmax,
                            field=field, retmode=retmode)
 
         # Initialize dictionary to save url arguments, and populate it from settings
         self.args = dict()
-        self.save_args()
+        self.fill_args()
+
+        # Check for authentication (API key)
+        if self.api_key:
+            self.authenticated = True
+            self.api_key = api_key
+        else:
+            self.authenticated = False
+            self.api_key = None
 
         if auto_gen:
-            self.build_search([])
-            self.build_fetch([])
+            self.build_url('search', [])
+            self.build_url('fetch', [])
 
 
-    def save_settings(self, usehistory=None, db=None, retmax=None, field=None, retmode=None):
-        """Save provided setting values into a dictionary object.
+    def auth_url(self, url):
+        """Add user & tool details to URL, if authenticated."""
+
+        if self.authenticated:
+            return url + '&api_key=' + self.api_key
+        else:
+            return url
+
+
+    def check_args(self, args_to_use):
+        """Checks whether the requested arguments are defined, so that they can be used.
+
+        Parameters
+        ----------
+        args_to_use : list of str
+            Requested arguments to check that they are defined.
+        """
+
+        # Check that all requested arguments are available. Catch and raise custom error if not.
+        try:
+            [self.args[arg] for arg in args_to_use]
+        except KeyError:
+            raise InconsistentDataError('Not all requested settings provided - can not proceed.')
+
+
+    def build_url(self, util, args):
+        """Build the URL for a specified e-utility, with provided arguments.
+
+        Parameters
+        ----------
+        util : {'info', 'query', 'search', 'fetch'}
+            Which e-utility to build the URL for.
+        args : list of str
+            Arguments to use to build the URL.
+        """
+
+        utils = {'info' : 'einfo.fcgi?',
+                 'query' : 'egquery.fcgi?',
+                 'search' : 'esearch.fcgi?',
+                 'fetch' : 'efetch.fcgi?'}
+
+        if util not in utils:
+            raise ValueError('Specified e-utility not understood.')
+
+        self.check_args(args_to_use)
+
+        url = self.auth_url(self.eutils + utils[util])
+
+        if util in ['query', 'search']:
+            url += '&term='
+
+        setattr(self, util, url)
+
+
+    def fill_settings(self, usehistory=None, db=None, retmax=None, field=None, retmode=None):
+        """Put all provided settings values into a dictionary object.
 
         Parameters
         ----------
@@ -133,148 +196,29 @@ class URLS(object):
         - The 'locals()' function returns a dictionary of variables in scope (in this function).
         - Using 'locals()' saves separately defining a list of possible variables, that
             would need to be maintained to make sure it matched the method arguments.
-
-        Equivalent and more explicit:
-
-        # Initialize list of possible settings, and remove the self argument
-        possible_settings = locals().keys()
-        possible_settings.remove('self')
-
-        # Loop through all possible settings
-        for ps in possible_settings:
-
-            # If defined (not None) set the value of the setting
-            #   into a dictionary, with key of the name of the setting
-            if eval(ps):
-                self.settings[ps] = eval(ps)
         """
 
-        # Save all defined settings to a dictionary
-        self.settings = {k: v for k, v in locals().items() if k is not 'self' and v is not None}
+        # # Equivalent and more explicit to the dictionary comprehension below
+        #
+        # # Initialize list of possible settings, and remove the self argument
+        # possible_settings = locals().keys()
+        # possible_settings.remove('self')
+        #
+        # # Loop through all possible settings
+        # for ps in possible_settings:
+        #
+        #     # If defined (not None) set the value of the setting
+        #     #   into a dictionary, with key of the name of the setting
+        #     if eval(ps):
+        #         self.settings[ps] = eval(ps)
+
+        self.settings = {ke: va for ke, va in locals().items() \
+            if ke is not 'self' and va is not None}
 
 
-    def save_args(self):
+    def fill_args(self):
         """Create the arguments that can be added to the e-utils urls."""
 
         # For each parameter in settings, create the url argument
         for param in self.settings.keys():
             self.args[param] = param + '=' + self.settings[param]
-
-
-    def check_args(self, args_to_use):
-        """Checks whether the requested arguments are defined, so that they can be used.
-
-        Parameters
-        ----------
-        args_to_use : list of str
-            Requested arguments to check that they are defined.
-        """
-
-        # Check that all requested arguments are available. Catch and raise custom error if not
-        try:
-            [self.args[arg] for arg in args_to_use]
-        except KeyError:
-            raise InconsistentDataError('Not all requested settings provided - can not proceed.')
-
-
-    def build_info(self, args_to_use):
-        """Create the e-utils info URL, with specified arguments.
-
-        Parameters
-        ----------
-        args_to_use : list of str
-            Arguments to use to build the info URL.
-        """
-
-        # Check requested args are defined in settings
-        self.check_args(args_to_use)
-
-        # Set the eg query search url
-        info_base = _check_auth(self.eutils + 'einfo.fcgi?')
-        self.info = info_base + '&'.join([self.args[arg] for arg in args_to_use])
-
-
-    def build_query(self, args_to_use):
-        """Create the e-utils EGQuery URL, with specified arguments.
-
-        Parameters
-        ----------
-        args_to_use : list of str
-            Arguments to use to build the query URL.
-        """
-
-        # Check requested args are defined in settings
-        self.check_args(args_to_use)
-
-        # Set the eg query search url
-        query_base = _check_auth(self.eutils + 'egquery.fcgi?')
-        self.query = query_base + '&'.join([self.args[arg] for arg in args_to_use]) + '&term='
-
-
-    def build_search(self, args_to_use):
-        """Create the e-utils search URL, with specified arguments.
-
-        Parameters
-        ----------
-        args_to_use : list of str
-            Arguments to use to build the search URL.
-        """
-
-        # Check requested args are defined in settings
-        self.check_args(args_to_use)
-
-        # Set the search url
-        search_base = _check_auth(self.eutils + 'esearch.fcgi?')
-        self.search = search_base + '&'.join([self.args[arg] for arg in args_to_use]) + '&term='
-
-
-    def build_fetch(self, args_to_use):
-        """Create the e-utils fetch URL, with specified arguments.
-
-        Parameters
-        ----------
-        args_to_use : list of str
-            Arguments to use to build the fetch URL.
-        """
-
-        # Check requested args are defined in settings
-        self.check_args(args_to_use)
-
-        # Set the fetch url
-        fetch_base = _check_auth(self.eutils + 'efetch.fcgi?')
-        self.fetch = fetch_base + '&'.join([self.args[arg] for arg in args_to_use])
-
-###################################################################################################
-###################################################################################################
-
-def _check_auth(url):
-    """Check for authorization, if so add registered details."""
-
-    if AUTH and _authenticate:
-        return url + 'email=tdonoghue@ucsd&tool=ERPscanr&'
-    else:
-        return url
-
-def _authenticate():
-    """Check that user has permission to run as registered E-Utils tool."""
-
-    import hashlib
-
-    try:
-        with open('.auth', 'r') as f:
-            pw = f.read()
-            pw = pw.strip('\n')
-
-        pwh = hashlib.sha224(pw.encode()).hexdigest()
-        chh = 'a28ffb33c70f372d8974c12424f605518f025c08e94663e705304298'
-
-        if  pwh == chh:
-            print('Authentification passed.')
-            return True
-        else:
-            print('Authentification failed.')
-            return False
-
-    except:
-        print('Authentification failed.')
-        return False
