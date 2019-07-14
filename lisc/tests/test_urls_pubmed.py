5af197c1db8a67bca833a143ead9e972e0a6a164
TomDonoghue <tdonoghue@ucsd.edu>
2019-07-13 21:32:37 -0700

    Update the URLs object

--- lisc/tests/test_urls_pubmed.py
+++ lisc/tests/test_urls_pubmed.py
@@ -2,31 +2,31 @@
 
 from py.test import raises
 
-from lisc.urls.pubmed import URLS
 from lisc.core.errors import InconsistentDataError
+
+from lisc.urls.pubmed import *
 
 ###################################################################################################
 ###################################################################################################
+
+def test_get_wait_time():
+
+    assert get_wait_time(True)
 
 def test_urls():
 
     assert URLS()
 
-def test_urls_settings_args():
+def test_urls_settings():
     """Tests URLS() returns properly with settings provided, and args defined.
     This triggers save_settings() and save_args() methods with inputs from __init__.
     """
 
-    assert URLS(db='pubmed', retmax='500', field='id', retmode='xml')
-
-def test_check_args():
-
-    urls = URLS(db='pubmed', field='id')
-
-    urls.check_args(['db', 'field'])
-
-    with raises(InconsistentDataError):
-        urls.check_args(['db', 'retmax', 'field'])
+    urls = URLS(db='pubmed', retmax=500, retmode='xml')
+    assert urls.settings['db'] == 'pubmed'
+    assert urls.settings['retmax'] == '500'
+    with raises(KeyError):
+        urls.settings['field']
 
 def test_build_url():
 
@@ -43,3 +43,25 @@ def test_build_url():
 
     urls.build_url('fetch', ['db', 'retmode'])
     assert urls.fetch
+
+def test_check_url():
+
+    urls = URLS(db='pmc')
+    urls.build_url('info', ['db'])
+
+    urls.check_url('info')
+    with raises(ValueError):
+        urls.check_url('notinfo')
+
+def test_get_url():
+
+    urls = URLS(db='pubmed', retmode='xml')
+
+    urls.build_url('info', ['db'])
+    info = urls.get_url('info')
+    assert 'info' in info
+
+    urls.build_url('fetch', ['db', 'retmode'])
+    fetch = urls.get_url('fetch', {'WebEnv' : 'DATA'})
+    assert 'fetch' in fetch
+    assert 'WebEnv' in fetch
