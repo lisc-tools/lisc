708a63e187c0d24b0bfda64382b7ddb9508e26f0
TomDonoghue <tdonoghue@ucsd.edu>
2019-07-16 13:45:45 -0700

    Extend some DB stuff

--- lisc/core/db.py
+++ lisc/core/db.py
@@ -85,6 +85,12 @@ class SCDB():
 
         return os.listdir(getattr(self, f_type + '_path'))
 
+
+    def check_file_structure(self):
+        """Check the file structure of the database."""
+
+        check_file_structure(self.base_path)
+
 ###################################################################################################
 ###################################################################################################
 
@@ -118,17 +124,53 @@ def check_folder(folder, f_type):
     return folder
 
 
-def create_file_structure(base_path):
+def create_file_structure(base_path=None, dir_name='lisc_db'):
     """Create the file structure for a SCANR database.
 
     Attributes
     ----------
-    base_path : str
+    base_path : str or None
         Base path for the project.
+        If none, the structure is created in th current directory.
+    dir_name : str
+        The name of the root folder of the directory structure.
+
+    Returns
+    -------
+    db : SCDB()
+        A database object for the file structure that was created.
     """
 
-    db = SCDB(base_path)
+    if not base_path:
+        base_path = os.getcwd()
+
+    db = SCDB(os.path.join(base_path, dir_name))
 
     for level in sorted(PATHS.keys()):
         for label in PATHS[level]:
             os.mkdir(getattr(db, label + '_path'))
+
+    return db
+
+
+def check_file_structure(base_path):
+    """Check the file structure of a folder.
+
+    Parameters
+    ----------
+    base_path : str
+        Base path of the directory structure to check.
+    """
+
+    for root, dirs, files in os.walk(base_path):
+
+        level = root.replace(base_path, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+
+        print('{}{}/'.format(indent, os.path.basename(root)))
+
+        subindent = ' ' * 4 * (level + 1)
+
+        for file in files:
+            if file[0] == '.': continue
+            print('{}{}'.format(subindent, file))
