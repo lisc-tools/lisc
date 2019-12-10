951f89844bf4c55611f414dd5a4b61bd2a72903b
TomDonoghue <tdonoghue@ucsd.edu>
2019-12-10 15:55:24 -0800

    Version bump & setup tweaks

--- setup.py
+++ setup.py
@@ -39,7 +39,10 @@ setup(
     long_description = long_description,
     author = 'Thomas Donoghue',
     author_email = 'tdonoghue.research@gmail.com',
+    maintainer = 'Thomas Donoghue',
+    maintainer_email = 'tdonoghue.research@gmail.com',
     url = 'https://github.com/lisc-tools/lisc',
+    download_url = 'https://github.com/lisc-tools/lisc/releases',
     packages = find_packages(),
     license = 'Apache License, 2.0',
     classifiers = [
@@ -47,22 +50,28 @@ setup(
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering',
         'License :: OSI Approved :: Apache Software License',
-        'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
         'Operating System :: Unix',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
         ],
-    download_url = 'https://github.com/lisc-tools/lisc/releases',
-    keywords = ['web-scraping', 'meta-analysis', 'text-mining', 'scientific-publications', 'literature-mining', 'literature-review'],
+
+    keywords = ['web-scraping', 'meta-analysis', 'text-mining', 'scientific-publications',
+                'literature-mining', 'literature-review'],
     install_requires = ['numpy', 'nltk', 'beautifulsoup4', 'requests', 'lxml'],
     tests_require = ['pytest'],
     extras_require = {
-        'plot'    : ['matplotlib', 'seaborn', 'wordcloud'],
-        'analysis'   : ['scipy'],
-        'all'     : ['matplotlib', 'seaborn', 'wordcloud', 'scipy']
-    }
+        'plot'     : ['matplotlib', 'seaborn', 'wordcloud'],
+        'analysis' : ['scipy'],
+        'all'      : ['matplotlib', 'seaborn', 'wordcloud', 'scipy']
+    },
+    project_urls = {
+        'Documentation' : 'https://lisc-tools.github.io/',
+        'Bug Reports' : 'https://github.com/lisc-tools/lisc/issues',
+        'Source' : 'https://github.com/lisc-tools/lisc'
+    },
 )
