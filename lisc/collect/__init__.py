eec938938ea2fbf1cdcd0fdaa65d806fbff8f3cb
Tom Donoghue <tdonoghue.research@gmail.com>
2021-01-22 12:33:58 -0500

    refactor to combine into collect_citations

--- lisc/collect/__init__.py
+++ lisc/collect/__init__.py
@@ -1,4 +1,4 @@
 from .info import collect_info
 from .words import collect_words
 from .counts import collect_counts
-from .citations import collect_citations, collect_citation_dois
+from .citations import collect_citations
--- lisc/collect/citations.py
+++ lisc/collect/citations.py
@@ -9,7 +9,8 @@ from lisc.urls.open_citations import OpenCitations
 ###################################################################################################
 ###################################################################################################
 
-def collect_citations(dois, util='citations', logging=None, directory=None, verbose=False):
+def collect_citations(dois, util='citations', collect_dois=False,
+                      logging=None, directory=None, verbose=False):
     """Collect citation data from OpenCitations.
 
     Parameters
@@ -30,7 +31,7 @@ def collect_citations(dois, util='citations', logging=None, directory=None, verb
 
     Returns
     -------
-    citations : dict
+    n_citations : dict
         The number of citations for each DOI.
     meta_data : MetaData
         Meta data about the data collection.
@@ -45,15 +46,25 @@ def collect_citations(dois, util='citations', logging=None, directory=None, verb
     if verbose:
         print('Collecting citation data.')
 
-    citations = {doi : get_citation_data(req, urls.get_url(util, [doi])) for doi in dois}
+    n_citations, citing_dois = {}, {}
+    for doi in dois:
+        outputs = get_citation_data(req, urls.get_url(util, [doi]), collect_dois=collect_dois)
+        if collect_dois:
+            n_citations[doi] = outputs[0]
+            citing_dois[doi] = outputs[1]
+        else:
+            n_citations[doi] = outputs
 
     meta_data.add_requester(req)
 
-    return citations, meta_data
+    if not collect_dois:
+        return n_citations, meta_data
+    else:
+        return n_citations, citing_dois, meta_data
 
 
-def get_citation_data(req, citation_url):
-    """Extract the number of citations from an OpenCitations URL request.
+def get_citation_data(req, citation_url, collect_dois=False):
+    """Extract citations from an OpenCitations URL request.
 
     Parameters
     ----------
@@ -66,76 +77,8 @@ def get_citation_data(req, citation_url):
     -------
     n_citations : int
         The number of citations the article has received.
-    """
-
-    page = req.request_url(citation_url)
-    n_citations = len(json.loads(page.content.decode('utf-8')))
-
-    # If the return is empty, encode as None instead of zero
-    #   This is because we don't want to treat missing data as 0 citations
-    if n_citations == 0:
-        n_citations = None
-
-    return n_citations
-
-
-def collect_citation_dois(dois, util='citations', logging=None, directory=None, verbose=False):
-    """Collect a list of DOIs citing the target(s) using OpenCitations.
-
-    Parameters
-    ----------
-    dois : list of str
-        DOIs to collect citation data for.
-    util : {'citations', 'references'}
-        Which utility to collect citation data with. Options:
-
-        * 'citations': collects the number of citations citing the specified DOI.
-        * 'references': collects the number of references cited by the specified DOI.
-    logging : {None, 'print', 'store', 'file'}, optional
-        What kind of logging, if any, to do for requested URLs.
-    directory : str or SCDB, optional
-        Folder or database object specifying the save location.
-    verbose : bool, optional, default: False
-        Whether to print out updates.
-
-    Returns
-    -------
-    citations : dict
-        The number of citations for each DOI.
-    meta_data : MetaData
-        Meta data about the data collection.
-    """
-
-    urls = OpenCitations()
-    urls.build_url(util)
-
-    meta_data = MetaData()
-    req = Requester(wait_time=0.1, logging=logging, directory=directory)
-
-    if verbose:
-        print('Collecting citation data.')
-
-    citations = {doi : get_citation_doi_data(req, urls.get_url(util, [doi])) for doi in dois}
-
-    meta_data.add_requester(req)
-
-    return citations, meta_data
-
-
-def get_citation_doi_data(req, citation_url):
-    """Extract the number of citations from an OpenCitations URL request.
-
-    Parameters
-    ----------
-    req : Requester
-        Requester to launch requests from.
-    citation_url : str
-        URL to collect citation data from.
-
-    Returns
-    -------
-   citations : list of strings
-        The DOIs of articles citing the target article.
+    citing_dois : list of str
+        The DOIs of the citing articles.
     """
 
     page = req.request_url(citation_url)
@@ -145,10 +88,12 @@ def get_citation_doi_data(req, citation_url):
     # If the return is empty, encode as None instead of zero
     #   This is because we don't want to treat missing data as 0 citations
     if n_citations == 0:
-        citations = None
+        n_citations = None
     else:
-        citations = [ acite['citing'] for acite in jpage]
+        if collect_dois:
+            citing_dois = [acite['citing'] for acite in jpage]
 
-    return citations
-
-
+    if not collect_dois:
+        return n_citations
+    else:
+        return n_citations, citing_dois
