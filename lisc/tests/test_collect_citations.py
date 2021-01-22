170d83c11cd02b2a0f76f4a3dbd4cfe332f52b27
Tom Donoghue <tdonoghue.research@gmail.com>
2021-01-22 18:04:04 -0500

    extend tests for new cite collects

--- lisc/tests/test_collect_citations.py
+++ lisc/tests/test_collect_citations.py
@@ -9,8 +9,14 @@ def test_collect_citations():
 
     dois = ['10.1007/s00228-017-2226-2', '10.1186/1756-8722-6-59']
 
-    citations, meta_data = collect_citations(dois, 'citations')
-    assert citations
+    # Test 'citations' collection
+    n_citations, cite_dois, meta_data = collect_citations(dois, 'citations', collect_dois=True)
+    assert len(n_citations) == len(cite_dois) == len(dois)
+    assert isinstance(list(n_citations.values())[0], int)
+    assert isinstance(list(cite_dois.values())[0][0], str)
 
-    references, meta_data = collect_citations(dois, 'references')
-    assert references
+    # Test 'references' collection
+    n_references, ref_dois, meta_data = collect_citations(dois, 'references', collect_dois=True)
+    assert len(n_references) == len(ref_dois) == len(dois)
+    assert isinstance(list(n_references.values())[0], int)
+    assert isinstance(list(ref_dois.values())[0][0], str)
