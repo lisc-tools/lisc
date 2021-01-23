86c164f94a3c109cb1469c9470384e8f2131617f
Tom Donoghue <tdonoghue.research@gmail.com>
2021-01-23 00:31:48 -0500

    add urls utils & tests

--- lisc/tests/urls/test_utils.py
+++ lisc/tests/urls/test_utils.py
@@ -5,6 +5,21 @@ from lisc.urls.utils import *
 ###################################################################################################
 ###################################################################################################
 
+def test_check_none():
+
+    out1 = check_none(None, 1)
+    assert out1 == 1
+
+    out2 = check_none(1, 2)
+    assert out2 == 1
+
+def test_check_settings():
+
+    settings = {'key1' : 'value1', 'key2' : 12}
+    output = check_settings(settings)
+    for val in output.values():
+        assert isinstance(val, str)
+
 def test_prepend():
 
     assert prepend('Test', '?') == '?Test'
