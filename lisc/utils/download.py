bc4ad73ca0008d19f3cb8cd0e5283036bce99f19
Tom Donoghue <tdonoghue.research@gmail.com>
2021-01-22 22:08:23 -0500

    fix spacing issue

--- lisc/utils/download.py
+++ lisc/utils/download.py
@@ -6,7 +6,7 @@ import nltk
 ###################################################################################################
 
 def download_nltk_data():
-	"""Download required nltk data for LISC analyses."""
+    """Download required nltk data for LISC analyses."""
 
     nltk.download('punkt')
     nltk.download('stopwords')
