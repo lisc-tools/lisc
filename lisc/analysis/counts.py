7af4fdea033d212e55d9d8f0413c9303f2c92510
Tom Donoghue <tdonoghue.research@gmail.com>
2021-01-25 19:56:54 -0500

    drop docstring examples in analysis

--- lisc/analysis/counts.py
+++ lisc/analysis/counts.py
@@ -25,16 +25,6 @@ def compute_normalization(data, counts, dim='A'):
     Notes
     -----
     This computes a normalized data matrix as a percent of articles expressing co-occurrence.
-
-    Examples
-    --------
-    Compute normalized co-occurrences between two lists of terms:
-
-    >>> from lisc.collect import collect_counts
-    >>> terms_a = [['frontal lobe'], ['parietal lobe']]
-    >>> terms_b = [['decision making'], ['sensory']]
-    >>> coocs, counts, meta_dat = collect_counts(terms_a=terms_a, terms_b=terms_b) # doctest:+SKIP
-    >>> normed_coocs = compute_normalization(coocs, counts[0]) # doctest:+SKIP
     """
 
     if dim == 'A':
@@ -80,16 +70,6 @@ def compute_association_index(data, counts_a, counts_b):
 
     The denominator, :math:`|c_{ij} U d_{ij}|`, is equivalent to
     :math:`|c_{ij}| + |d_{ij}| - |c_{ij} N d_{ij}|`.
-
-    Examples
-    --------
-    Compute the association index between two lists of terms:
-
-    >>> from lisc.collect import collect_counts
-    >>> terms_a = [['frontal lobe'], ['parietal lobe']]
-    >>> terms_b = [['decision making'], ['sensory']]
-    >>> coocs, counts, meta_dat = collect_counts(terms_a=terms_a, terms_b=terms_b) # doctest:+SKIP
-    >>> index = compute_association_index(coocs, counts[0], counts[1]) # doctest:+SKIP
     """
 
     n_a = len(counts_a)
