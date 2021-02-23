5075900fb4d6b789b9af9cb06b494c400f893af5
Tom Donoghue <tdonoghue.research@gmail.com>
2021-02-23 17:44:39 -0500

    reorg & extend metadata tests

--- lisc/tests/data/test_meta_data.py
+++ lisc/tests/data/test_meta_data.py
@@ -1,18 +1,30 @@
 """Tests for lisc.data.meta_data."""
 
+from datetime import datetime
+
 from lisc.data.meta_data import *
 
 ###################################################################################################
 ###################################################################################################
 
-def test_meta_data(treq):
+def test_meta_data():
 
     meta_data = MetaData()
     assert meta_data
     assert meta_data['date']
 
-    meta_data.add_requester(treq)
-    assert meta_data.requester
+def test_meta_data_get_date(tmetadata):
 
-    meta_data.add_db_info({'db' : 'name'})
-    assert meta_data.db_info
+	tmetadata.get_date()
+	assert isinstance(tmetadata.date, str)
+	assert tmetadata.date[0:4] == str(datetime.now().year)
+
+def test_meta_data_add_requester(treq)
+
+    tmetadata.add_requester(treq)
+    assert tmetadata.requester
+
+def test_meta_data_add_db_info(tmetadata):
+
+    tmetadata.add_db_info({'db' : 'name'})
+    assert tmetadata.db_info
