766e9b8342f0c9a745d2e4eaf2c47c8fa4ed1b4e
Tom Donoghue <tdonoghue.research@gmail.com>
2021-02-23 18:27:35 -0500

    add tests for as_dict

--- lisc/tests/data/test_meta_data.py
+++ lisc/tests/data/test_meta_data.py
@@ -15,33 +15,31 @@ def test_meta_data():
 
 def test_meta_data_get_date(tmetadata):
 
-	tmetadata.get_date()
-	assert isinstance(tmetadata.date, str)
-	assert tmetadata.date[0:4] == str(datetime.now().year)
+    tmetadata.get_date()
+    assert isinstance(tmetadata.date, str)
+    assert tmetadata.date[0:4] == str(datetime.now().year)
 
-def test_meta_data_add_requester(treq)
+def test_meta_data_add_requester(tmetadata, treq):
 
     tmetadata.add_requester(treq)
     assert tmetadata.requester
 
 def test_meta_data_add_db_info(tmetadata):
 
-    tmetadata.add_db_info({'db' : 'name'})
+    tmetadata.add_db_info({'dbname' : 'name'})
     assert tmetadata.db_info
 
 def test_meta_data_as_dict(tmetadata, treq):
 
-	mt_dict = tmetadata.as_dict()
-	assert isinstance(mt_dict, dict)
+    mt_dict = tmetadata.as_dict()
+    assert isinstance(mt_dict, dict)
 
-	# Test with db info added
-	tmetadata.add_db_info({'db' : 'name'})
+    # Test with db info added
+    tmetadata.add_db_info({'dbname' : 'name'})
+    mt_dict = tmetadata.as_dict()
+    assert 'db_info_dbname' in mt_dict
 
-	# Test with a requester added
-	tmetadata.add_requester(treq)
-	mt_dict = tmetadata.as_dict()
-
-
-
-
-
+    # Test with a requester added
+    tmetadata.add_requester(treq)
+    mt_dict = tmetadata.as_dict()
+    assert 'requester_n_requests' in mt_dict
