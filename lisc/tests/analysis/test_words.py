14c67ceb8351ed0843a65c1f8d13d9c9d8997c4b
Tom Donoghue <tdonoghue.research@gmail.com>
2021-05-15 23:09:41 -0400

    update words analysis funcs

--- lisc/tests/analysis/test_words.py
+++ lisc/tests/analysis/test_words.py
@@ -7,15 +7,32 @@ from lisc.analysis.words import *
 ###################################################################################################
 ###################################################################################################
 
-def test_get_all_values(tarts_all):
+def test_get_attribute_counts(twords_full, tarts_full, tarts_none):
 
-    lst = [tarts_all, tarts_all]
+    attr = 'dois'
 
+    count = get_attribute_counts(twords_full, attr)
+    assert count == sum([len(getattr(res, attr)) for res in twords_full.results])
+
+    count = get_attribute_counts([tarts_full, tarts_full], attr)
+    assert count == len(getattr(tarts_full, attr)) * 2
+
+    count = get_attribute_counts([tarts_none, tarts_none], attr)
+    assert count == len(getattr(tarts_none, attr)) * 2 - 2
+
+
+def test_get_all_values(twords_full, tarts_full, tarts_none):
+
+    attr = 'dois'
+
+    values = get_all_values(twords_full, attr)
+
+    lst = [tarts_full, tarts_full]
     vals = get_all_values(lst, 'dois')
-    assert len(vals) == 2*len(tarts_all.dois)
+    assert len(vals) == 2 * len(tarts_full.dois)
 
     vals = get_all_values(lst, 'dois', unique=True)
-    assert len(vals) == len(set(tarts_all.dois))
+    assert len(vals) == len(set(tarts_full.dois))
 
 def test_get_all_counts(tarts_all):
 
