c39d233d4ed732672ca95df153a70ce179e0a3d2
Tom Donoghue <tdonoghue.research@gmail.com>
2022-11-28 23:54:01 -0500

    list 3.11 support in setup

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         ],
     platforms = 'any',
     keywords = ['web-scraping', 'meta-analysis', 'text-mining', 'scientific-publications',
