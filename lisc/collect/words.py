d109a70a351c4b774cd7b0a4d9f00879cb53e46e
hahaschool <hahaschool.wing@gmail.com>
2023-06-28 21:14:14 +0800

    feat: add URL batching

--- lisc/collect/words.py
+++ lisc/collect/words.py
@@ -165,8 +165,16 @@ def collect_words(terms, inclusions=None, exclusions=None, labels=None,
 
             ids = page_soup.find_all('id')
             ids_str = ','.join([el.text for el in ids])
-            art_url = urls.get_url('fetch', settings={'id' : ids_str})
-            arts = get_articles(req, art_url, arts)
+
+            arts = []
+
+            # Batch ids_str into groups of 100 to avoid URL length limit
+            for i in range(0, len(ids_str), 100):
+                batch = ids_str[i:i+100]
+                url_settings = {'id' : batch}
+                art_url = urls.get_url('fetch', settings=url_settings)
+                arts = get_articles(req, art_url, arts)
+
 
         arts._check_results()
 
