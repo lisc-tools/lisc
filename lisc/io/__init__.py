40cca19024ef2384560952d969cbbe245c917b45
Tom Donoghue <tdonoghue.research@gmail.com>
2023-09-10 16:37:40 -0400

    fix init import of time results IO funcs

--- lisc/io/__init__.py
+++ lisc/io/__init__.py
@@ -2,4 +2,4 @@
 
 from .db import SCDB, create_file_structure
 from .io import (save_object, load_object, load_api_key, load_txt_file,
-                 save_meta_data, load_meta_data)
+                 save_time_results, load_time_results, save_meta_data, load_meta_data)
