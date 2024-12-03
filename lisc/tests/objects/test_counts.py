240564841a41dfdb50fbf9aae3545283c890854c
Tom Donoghue <tdonoghue.research@gmail.com>
2024-12-03 13:32:51 -0500

    udpate counts tests

--- lisc/tests/objects/test_counts.py
+++ lisc/tests/objects/test_counts.py
@@ -5,42 +5,40 @@ from lisc.objects.counts import Counts1D, Counts
 ###################################################################################################
 ###################################################################################################
 
-## Helper test functions
+## Helper test functions for Counts object
 
 def check_dunders(counts):
 
-    if isinstance(counts, Counts1D):
+    label0 = counts.terms['A'].labels[0]
+    label1 = counts.terms['B' if counts.terms['B'].terms else 'A'].labels[0]
 
-        label = counts.labels[0]
-        out = counts[label0]
-
-        assert out == counts.counts[0]
-
-    if isinstance(counts, Counts):
-
-        label0 = counts.terms['A'].labels[0]
-        label1 = counts.terms['B' if counts.terms['B'].terms else 'A'].labels[0]
-
-        out = counts[label0, label1]
-        assert out == counts.counts[(0, 0)]
+    out = counts[label0, label1]
+    assert out == counts.counts[(0, 0)]
 
 def check_funcs(counts):
 
-    if isinstance(counts, Counts):
-        counts.check_data()
-    counts.check_top()
-    counts.check_counts()
+    for dim in ['A'] if counts.square else ['A', 'B', 'both']:
+        counts.check_data(dim=dim)
+        counts.check_top(dim=dim)
+        counts.check_counts(dim=dim)
 
 def drop_data(counts):
 
-    counts.drop_data(0)
+    for dim in ['A'] if counts.square else ['A', 'B', 'both']:
+        counts.drop_data(10, dim=dim)
 
 def compute_scores(counts):
 
-    for score_type in ['normalize', 'association', 'similarity']:
-        counts.compute_score(score_type)
-        assert counts.score.any()
-        assert counts.score_info['type'] == score_type
+    counts.compute_score('association')
+    assert counts.score.any()
+    assert counts.score_info['type'] == 'association'
+
+    for score_type in ['similarity', 'normalize']:
+        for dim in ['A'] if counts.square else ['A', 'B']:
+            counts.compute_score(score_type, dim=dim)
+            assert counts.score.any()
+            assert counts.score_info['type'] == score_type
+            assert counts.score_info['dim'] == dim
 
 ## Counts1D Object
 
@@ -57,8 +55,12 @@ def test_collect(test_req):
 
     assert counts.has_data
 
-    check_funcs(counts)
-    drop_data(counts)
+    assert counts[counts.labels[0]] == counts.counts[0]
+
+    counts.check_top()
+    counts.check_counts()
+
+    counts.drop_data(10)
 
 ## Counts Object
 
@@ -78,7 +80,7 @@ def test_collect_one(test_req):
     counts = Counts()
 
     counts.add_terms(['language', 'memory'], dim='A')
-    counts.add_terms(['protein', 'protein'], term_type='exclusions', dim='A')
+    counts.add_terms(['protein', 'cell'], term_type='exclusions', dim='A')
 
     counts.run_collection(db='pubmed', logging=test_req)
     assert counts.has_data
@@ -94,7 +96,7 @@ def test_collect_two(test_req):
     counts = Counts()
 
     counts.add_terms(['language', 'memory'], dim='A')
-    counts.add_terms(['protein', 'protein'], term_type='exclusions', dim='A')
+    counts.add_terms(['protein', 'cell'], term_type='exclusions', dim='A')
     counts.add_terms(['cognition'], dim='B')
 
     counts.run_collection(db='pubmed', logging=test_req)
